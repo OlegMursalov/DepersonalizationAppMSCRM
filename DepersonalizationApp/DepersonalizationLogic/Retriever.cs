124467ae989b45daacc58275bc01988b157bd7e6
Oleg Mursalov <omursalov@testcrm.local>
2020-02-27 17:32:05 +0300

    distinct and fixes

--- DepersonalizationApp/DepersonalizationLogic/Retriever.cs
+++ DepersonalizationApp/DepersonalizationLogic/Retriever.cs
@@ -25,11 +25,11 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
             if (retrievedOpportunityLinks != null && retrievedOpportunityLinks.Count() > 0)
             {
-                var retrievedOpportunityIds = retrievedOpportunityLinks.Select(e => e.Id);
+                var retrievedOpportunityIds = retrievedOpportunityLinks.Select(e => e.Id).Distinct();
                 allRetrieved["opportunity"].AddRange(retrievedOpportunityIds);
 
                 var partOfOwnerRetriever = new CmdsoftPartOfOwnerRetriever(_sqlConnection, retrievedOpportunityIds);
-                var retrievedPartOfOwnerIds = partOfOwnerRetriever.Process();
+                var retrievedPartOfOwnerIds = partOfOwnerRetriever.Process().Distinct();
 
                 if (retrievedPartOfOwnerIds != null && retrievedPartOfOwnerIds.Count() > 0)
                 {
@@ -37,14 +37,14 @@ namespace DepersonalizationApp.DepersonalizationLogic
                 }
 
                 var orderNavRetriever = new CmdsoftOrderNavRetriever(_sqlConnection, retrievedOpportunityIds);
-                var retrievedOrderNavIds = orderNavRetriever.Process();
+                var retrievedOrderNavIds = orderNavRetriever.Process().Distinct();
 
                 if (retrievedOrderNavIds != null && retrievedOrderNavIds.Count() > 0)
                 {
                     allRetrieved["cmdsoft_ordernav"].AddRange(retrievedOrderNavIds);
 
                     var orderLineNavRetriever = new CmdsoftOrderLineNavRetriever(_sqlConnection, retrievedOrderNavIds);
-                    var retrievedOrderLineNavIds = orderLineNavRetriever.Process();
+                    var retrievedOrderLineNavIds = orderLineNavRetriever.Process().Distinct();
 
                     if (retrievedOrderLineNavIds != null && retrievedOrderLineNavIds.Count() > 0)
                     {
@@ -57,8 +57,8 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
                 if (retrievedAccountLinks != null && retrievedAccountLinks.Count() > 0)
                 {
-                    allRetrieved["account"].AddRange(retrievedAccountLinks.Select(e => e.Id));
-                    allRetrieved["contact"].AddRange(retrievedAccountLinks.Where(e => e.PrimaryContactId != null).Select(e => e.PrimaryContactId.Value));
+                    allRetrieved["account"].AddRange(retrievedAccountLinks.Select(e => e.Id).Distinct());
+                    allRetrieved["contact"].AddRange(retrievedAccountLinks.Where(e => e.PrimaryContactId != null).Select(e => e.PrimaryContactId.Value).Distinct());
                 }
 
                 var contactRetriever = new ContactRetriever(_sqlConnection, retrievedOpportunityLinks);
@@ -66,8 +66,8 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
                 if (retrievedContactLinks != null && retrievedContactLinks.Count() > 0)
                 {
-                    allRetrieved["contact"].AddRange(retrievedContactLinks.Select(e => e.Id));
-                    allRetrieved["account"].AddRange(retrievedContactLinks.Where(e => e.ParentCustomerId != null).Select(e => e.ParentCustomerId.Value));
+                    allRetrieved["contact"].AddRange(retrievedContactLinks.Select(e => e.Id).Distinct());
+                    allRetrieved["account"].AddRange(retrievedContactLinks.Where(e => e.ParentCustomerId != null).Select(e => e.ParentCustomerId.Value).Distinct());
                 }
 
                 var specificationRetriever = new CmdsoftSpecificationRetriever(_sqlConnection, retrievedOpportunityIds);
@@ -75,11 +75,11 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
                 if (retrievedSpecificationLinks != null && retrievedSpecificationLinks.Count() > 0)
                 {
-                    var retrievedSpecificationIds = retrievedSpecificationLinks.Select(e => e.Id);
+                    var retrievedSpecificationIds = retrievedSpecificationLinks.Select(e => e.Id).Distinct();
                     allRetrieved["cmdsoft_specification"].AddRange(retrievedSpecificationIds);
 
                     var listSpecificationRetriever = new CmdsoftListSpecificationRetriever(_sqlConnection, retrievedSpecificationIds);
-                    var retrievedListSpecificationIds = listSpecificationRetriever.Process();
+                    var retrievedListSpecificationIds = listSpecificationRetriever.Process().Distinct();
 
                     if (retrievedListSpecificationIds != null && retrievedListSpecificationIds.Count() > 0)
                     {
@@ -87,21 +87,21 @@ namespace DepersonalizationApp.DepersonalizationLogic
                     }
                     
                     var offerRetriever = new CmdsoftOfferRetriever(_sqlConnection, retrievedSpecificationIds);
-                    var retrievedOfferIds = offerRetriever.Process();
+                    var retrievedOfferIds = offerRetriever.Process().Distinct();
 
                     if (retrievedOfferIds != null && retrievedOfferIds.Count() > 0)
                     {
                         allRetrieved["cmdsoft_offer"].AddRange(retrievedOfferIds);
                     }
                     
-                    var retrievedSalesPriceIds = retrievedSpecificationLinks.Where(e => e.YolvaSalesPrice != null).Select(e => e.YolvaSalesPrice.Value);
+                    var retrievedSalesPriceIds = retrievedSpecificationLinks.Where(e => e.YolvaSalesPrice != null).Select(e => e.YolvaSalesPrice.Value).Distinct();
                     
                     if (retrievedSalesPriceIds != null && retrievedSalesPriceIds.Count() > 0)
                     {
                         allRetrieved["yolva_salesprice"].AddRange(retrievedSalesPriceIds);
 
                         var salespriceLineRetriever = new YolvaSalespriceLineRetriever(_sqlConnection, retrievedSalesPriceIds);
-                        var retrievedSalespriceLineIds = salespriceLineRetriever.Process();
+                        var retrievedSalespriceLineIds = salespriceLineRetriever.Process().Distinct();
 
                         if (retrievedSalespriceLineIds != null && retrievedSalespriceLineIds.Count() > 0)
                         {
@@ -112,7 +112,7 @@ namespace DepersonalizationApp.DepersonalizationLogic
             }
 
             var eventRetriever = new McdsoftEventRetriever(_sqlConnection);
-            var retrievedEventIds = eventRetriever.Process();
+            var retrievedEventIds = eventRetriever.Process().Distinct();
 
             if (retrievedEventIds != null && retrievedEventIds.Count() > 0)
             {
@@ -123,7 +123,7 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
                 if (retrievedEventsParticipantsLinks != null && retrievedEventsParticipantsLinks.Count() > 0)
                 {
-                    var retrievedEventsParticipantsIds = retrievedEventsParticipantsLinks.Select(e => e.Id);
+                    var retrievedEventsParticipantsIds = retrievedEventsParticipantsLinks.Select(e => e.Id).Distinct();
                     allRetrieved["yolva_events_participants"].AddRange(retrievedEventsParticipantsIds);
 
                     var accountRetriever = new AccountRetriever(_sqlConnection, retrievedEventsParticipantsLinks);
@@ -131,8 +131,8 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
                     if (retrievedAccountLinks != null && retrievedAccountLinks.Count() > 0)
                     {
-                        allRetrieved["account"].AddRange(retrievedAccountLinks.Select(e => e.Id));
-                        allRetrieved["contact"].AddRange(retrievedAccountLinks.Where(e => e.PrimaryContactId != null).Select(e => e.PrimaryContactId.Value));
+                        allRetrieved["account"].AddRange(retrievedAccountLinks.Select(e => e.Id).Distinct());
+                        allRetrieved["contact"].AddRange(retrievedAccountLinks.Where(e => e.PrimaryContactId != null).Select(e => e.PrimaryContactId.Value).Distinct());
                     }
 
                     var contactRetriever = new ContactRetriever(_sqlConnection, retrievedEventsParticipantsLinks);
@@ -140,17 +140,20 @@ namespace DepersonalizationApp.DepersonalizationLogic
 
                     if (retrievedContactLinks != null && retrievedContactLinks.Count() > 0)
                     {
-                        allRetrieved["contact"].AddRange(retrievedContactLinks.Select(e => e.Id));
-                        allRetrieved["account"].AddRange(retrievedContactLinks.Where(e => e.ParentCustomerId != null).Select(e => e.ParentCustomerId.Value));
+                        allRetrieved["contact"].AddRange(retrievedContactLinks.Select(e => e.Id).Distinct());
+                        allRetrieved["account"].AddRange(retrievedContactLinks.Where(e => e.ParentCustomerId != null).Select(e => e.ParentCustomerId.Value).Distinct());
                     }
                 }
             }
 
             var salesAppealRetriever = new McdsoftSalesAppealRetriever(_sqlConnection);
-            var salesAppealLinks = salesAppealRetriever.Process();
+            var retrievedSalesAppealLinks = salesAppealRetriever.Process();
 
-            if (salesAppealLinks != null && salesAppealLinks.Count() > 0)
+            if (retrievedSalesAppealLinks != null && retrievedSalesAppealLinks.Count() > 0)
             {
+                var retrievedSalesAppealIds = retrievedSalesAppealLinks.Select(e => e.Id).Distinct();
+                allRetrieved["mcdsoft_sales_appeal"].AddRange(retrievedSalesAppealIds);
+
                 
             }
         }
